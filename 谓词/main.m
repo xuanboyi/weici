2588b20758476768f8bb23241d05d09c694bbf9e
玄博屹 <genhiroshi@xuanboyidemacbook-pro.local>
2017-12-18 09:14:49 +0800

    Initial Commit

---
+++ 谓词/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  谓词
+//
+//  Created by 玄博屹 on 2017/12/18.
+//  Copyright © 2017年 玄博屹. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
